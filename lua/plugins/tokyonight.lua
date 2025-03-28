33bf9943823bb175d034f84ba96c4687decd0346
Evgeniy Kovalev <extern.evgeniy_kovalev@allianz.de>
2025-03-28 18:15:53 +0100

    UI

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -6,5 +6,12 @@ return {
       sidebars = "transparent",
       floats = "transparent",
     },
+    on_highlights = function(hl)
+      hl.comment = { fg = "#aaaaaa" }
+      hl.Comment = { fg = "#aaaaaa" }
+      hl.LineNrAbove = { fg = "#aaaaaa" }
+      hl.LineNrBelow = { fg = "#aaaaaa" }
+      -- hl.CursorLineNr = { fg = "#aaaaaa", bold = true }
+    end,
   },
 }
